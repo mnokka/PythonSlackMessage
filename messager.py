05eead5c937a88fc3589e7c80d2007d51bddc76e
Mika Nokka <mika.nokka@unikie.com>
2023-04-05 16:18:34 +0300

    Added slacktoken usage from env variable

--- messager.py
+++ messager.py
@@ -1,16 +1,25 @@
 #
 # 5.4.2023 mika.nokka1@gmail.com
 #
-# from https://howchoo.com/python/python-send-slack-messages-slackclient
+# Idea from https://howchoo.com/python/python-send-slack-messages-slackclient
+# 
 # pip install slackclient
 # see web page for Slack app creation/configuration magic
 #
-# TODO: add CLI interface and token storing system
+# ENV variable SLACKTOKEN assumed to include token for Slack app usage
+# (export SLACKTOKEN=MY_SLACK_GIVEN_TOKEN_TO_MY_APP)
+#
+# TODO: add CLI interface, trap possible slacking error
 
 import slack
+import os,sys
 
-mytoken="INSERT_MY_APP_SECRET_TOKEN"
-
+if 'SLACKTOKEN' in os.environ:
+    print("OK , env SLACKTOKEN found")
+    mytoken=(os.environ['SLACKTOKEN'])
+else:
+    print("ERROR!! No SLACKTOKEN env defined! EXITING")
+    sys.exit(5)
 
 client = slack.WebClient(token=mytoken)
 client.chat_postMessage(channel='test-messaging-apps', text='TESTING ONLY VIA PYTHON SCRIPT')
